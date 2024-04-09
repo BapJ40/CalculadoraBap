b26a33f1dd2d1bd57673f208de849d173f332d8c
Jose <gjr776@gmail.com>
2024-04-08 23:51:19 -0400

    Se agrego la funcion de sumar

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const MAX_LENGTH = 16;
 let numeroActual = "";
+let primerValor;
+let contador = 0;
 
 const boton1 = document.getElementById("boton1");
 const boton2 = document.getElementById("boton2");
@@ -12,82 +14,230 @@ const boton8 = document.getElementById("boton8");
 const boton9 = document.getElementById("boton9");
 const boton0 = document.getElementById("boton0");
 const botonPunto = document.getElementById("botonPunto"); 
-const botonSuma = document.getElementById("suma")
 const numerosPantalla = document.getElementById("numerosPantalla");
+const numerosPantallaDos = document.getElementById("numerosPantallaDos");
+const botonSuma = document.getElementById("suma");
 
-boton1.addEventListener("click", () => {
-    if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "1";
-        numerosPantalla.innerHTML = numeroActual;
-    }
-  });
+function suma(primerValor, segundoValor){
+  return Number(primerValor) + Number(segundoValor)
+}
 
-    boton2.addEventListener("click", () => {
-    if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "2";
-        numerosPantalla.innerHTML = numeroActual;
-    }
-  });
+botonSuma.addEventListener("click", function(){
+  function agreSuma(){
+    primerValor = Number(numeroActual);
+    numerosPantallaDos.innerHTML += " + ";
+  }
+  
+  if(contador === 0){
+    agreSuma()
+    numeroActual = ""
+    numerosPantalla.innerHTML = numeroActual;
 
-    boton3.addEventListener("click", () => {
-    if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "3";
-        numerosPantalla.innerHTML = numeroActual;
-    }
-  });
+  } else if (contador === 1) {
+    segundoValor = Number(numeroActual)
+    resuSuma  = suma(primerValor, segundoValor)
+    numerosPantalla.innerHTML = resuSuma;
+  }
 
-    boton4.addEventListener("click", () => {
-    if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "4";
-        numerosPantalla.innerHTML = numeroActual;
-    }
-  });
+  contador++
+}
+)
 
-  boton5.addEventListener("click", () => {
+boton1.addEventListener("click", function() {
+  if(contador === 0){
     if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "5";
-        numerosPantalla.innerHTML = numeroActual;
+      numeroActual += "1";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }} else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "1";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
     }
-  });
+  }
+  
+});
 
-  boton6.addEventListener("click", () => {
+boton2.addEventListener("click", function() {
+  if (contador === 0) {
     if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "6";
-        numerosPantalla.innerHTML = numeroActual;
+      numeroActual += "2";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "2";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
     }
-  });
+  }
+  
+});
 
-  boton7.addEventListener("click", () => {
+boton3.addEventListener("click", function() {
+  if (contador === 0) {
     if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "7";
-        numerosPantalla.innerHTML = numeroActual;
+      numeroActual += "3";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "3";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
     }
-  });
+  }
+  
+});
 
-  boton8.addEventListener("click", () => {
+boton4.addEventListener("click", function() {
+  if (contador === 0) {
     if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "8";
-        numerosPantalla.innerHTML = numeroActual;
+      numeroActual += "4";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "4";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
     }
-  });
+  }
+  
+});
 
-  boton9.addEventListener("click", () => {
+boton5.addEventListener("click", function() {
+  if (contador === 0) {
     if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "9";
-        numerosPantalla.innerHTML = numeroActual;
+      numeroActual += "5";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "5";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
     }
-  });
+  }
+  
+});
 
-  boton0.addEventListener("click", () => {
+boton6.addEventListener("click", function() {
+  if (contador === 0) {
     if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += "0";
-        numerosPantalla.innerHTML = numeroActual;
+      numeroActual += "6";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "6";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
     }
-  });
+  }
+  
+});
 
-  botonPunto.addEventListener("click", () => {
+boton7.addEventListener("click", function() {
+  if (contador === 0) {
     if ( numeroActual.length < MAX_LENGTH){
-        numeroActual += ".";
-        numerosPantalla.innerHTML = numeroActual;
+      numeroActual += "7";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "7";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
     }
-  });
+  }
+  
+});
+
+boton8.addEventListener("click", function() {
+  if (contador === 0) {
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "8";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "8";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+    }
+  }
+  
+});
+
+boton9.addEventListener("click", function() {
+  if (contador === 0) {
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "9";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "9";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+    }
+  }
+  
+});
+
+boton0.addEventListener("click", function() {
+  if (contador === 0) {
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "0";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += "0";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+    }
+  }
+  
+});
+
+botonPunto.addEventListener("click", function() {
+  if (contador === 0) {
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += ".";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+      primerValor = numerosPantallaDos
+  }
+  }else if (contador === 1){
+    if ( numeroActual.length < MAX_LENGTH){
+      numeroActual += ".";
+      numerosPantalla.innerHTML = numeroActual;
+      numerosPantallaDos.innerHTML = numeroActual;
+    }
+  }
+  
+});
+
+console.log(numerosPantalla, numerosPantallaDos, primerValor)
